a0f0efc6afe813eab821ae4b190ab22ff5c8e8e1
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2024-02-17 02:07:23 +0530

    [update] : Home page updated

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { URL_PATHS } from "../../constants";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="p-2">
+      <h1 className="text-2xl text-center ">React Questions</h1>
+      <div className="mt-4 flex flex-col gap-3 text-blue-500">
+        <Link to={URL_PATHS.PROBLEM1} className="hover:underline">
+          Problem 1
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
